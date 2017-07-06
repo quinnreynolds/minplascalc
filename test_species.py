d3b750c8589494ddff58f9c3f33ce398dde76c20
Carl Sandrock <carl.sandrock@up.ac.za>
2017-07-06 15:25:07 +0200

    Parametrize tests

--- test_species.py
+++ test_species.py
@@ -26,22 +26,21 @@ def test_internal_partition_function(diatomic_sample_species):
     partition_function = diatomic_sample_species.internalPartitionFunction(300)
     assert partition_function == pytest.approx(217.6606)
 
+@pytest.mark.parametrize("temperature, energy, tol", [
+    (1000, 2.070973e-20, 1e-26),
+    (25000, 7.619840e-19, 1e-25),
+])
+def test_monatomic_internal_energy(monatomic_sample_species,
+                                   temperature, energy, tol):
+    internal_energy = monatomic_sample_species.internal_energy(temperature)
+    assert internal_energy == pytest.approx(energy, abs=tol)
 
-def test_monatomic_internal_energy_lowt(monatomic_sample_species):
-    internal_energy = monatomic_sample_species.internal_energy(1000)
-    assert internal_energy == pytest.approx(2.070973e-20, abs=1e-26)
-    
+@pytest.mark.parametrize("temperature, energy, tol", [
+    (1000, 3.811852e-20, 1e-26),
+    (25000, 1.192611e-18, 1e-24),
+])
+def test_diatomic_internal_energy(diatomic_sample_species,
+                                  temperature, energy, tol):
+    internal_energy = diatomic_sample_species.internal_energy(temperature)
+    assert internal_energy == pytest.approx(energy, abs=tol)
 
-def test_monatomic_internal_energy_hight(monatomic_sample_species):
-    internal_energy = monatomic_sample_species.internal_energy(25000)
-    assert internal_energy == pytest.approx(7.619840e-19, abs=1e-25)
-
-
-def test_diatomic_internal_energy_lowt(diatomic_sample_species):
-    internal_energy = diatomic_sample_species.internal_energy(1000)
-    assert internal_energy == pytest.approx(3.811852e-20, abs=1e-26)
-
-
-def test_diatomic_internal_energy_hight(diatomic_sample_species):
-    internal_energy = diatomic_sample_species.internal_energy(25000)
-    assert internal_energy == pytest.approx(1.192611e-18, abs=1e-24)
