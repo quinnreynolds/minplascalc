Corrected ionisation energy lowering formula

minplascalc/__init__.py
@@ -531,7 +531,9 @@ class Mixture:
             if sp.name != "e":
                 ai = (3. * (sp.chargeNumber + 1.)
                       / (4. * np.pi * (ni[-1] / V))) ** (1 / 3)
-                sp.deltaIonisationEnergy = constants.boltzmann * T * (((ai / debyeD) ** (2 / 3) + 1) - 1) / (2. * (zStar + 1))
+                sp.deltaIonisationEnergy = (constants.boltzmann * T * 
+                                            (((ai / debyeD) + 1) ** (2 / 3) - 1)
+                                            / (2. * (zStar + 1)))
 
         for cn in range(1, self.maxChargeNumber + 1):
             for sp in self.species: