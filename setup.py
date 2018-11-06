c4df2db32a4265b6dfe87ad0740f3dbb7f5204b8
Carl Sandrock <carl.sandrock@up.ac.za>
2018-11-06 12:32:27 +0200

    Add nbconvert to test requirements

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     # $ pip install -e .[dev,test]
     extras_require={
         'dev': ['matplotlib'],
-        'test': ['coverage', 'pytest', 'nbformat'],
+        'test': ['coverage', 'pytest', 'nbformat', 'nbconvert'],
     },
 
     # If there are data files included in your packages that need to be
