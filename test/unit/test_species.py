0e63f852add08e6dca67b13ce01189761f48d0d3
Quinn Reynolds <quinnr@mintek.co.za>
2022-04-24 17:35:53 +0200

    Update unit test results

--- test/unit/test_species.py
+++ test/unit/test_species.py
@@ -19,7 +19,7 @@ def test_partitionfunction_translational(diatomic_sample_species):
 
 def test_partitionfunction_internal(diatomic_sample_species):
     partition_function = diatomic_sample_species.partitionfunction_internal(300, 0)
-    assert partition_function == pytest.approx(217.6606)
+    assert partition_function == pytest.approx(4.921932)
 
 @pytest.mark.parametrize('T, energy, tol', [
     (1000, 2.070973e-20, 1e-26),
@@ -31,8 +31,8 @@ def test_monatomic_internal_energy(monatomic_sample_species,
     assert internal_energy == pytest.approx(energy, abs=tol)
 
 @pytest.mark.parametrize('T, energy, tol', [
-    (1000, 3.811854e-20, 1e-26),
-    (25000, 1.192611e-18, 1e-24),
+    (1000, 5.381335e-20, 1e-26),
+    (25000, 1.208306e-18, 1e-24),
 ])
 def test_diatomic_internal_energy(diatomic_sample_species,
                                   T, energy, tol):
