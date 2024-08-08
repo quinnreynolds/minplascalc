f23be7a8f9d2f14ee6c894bac288b91b24fff91b
Quinn Reynolds <quinn.reynolds@gmail.com>
2024-08-08 14:19:30 +0200

    Update repr for species

--- minplascalc/species.py
+++ minplascalc/species.py
@@ -337,7 +337,7 @@ class Diatomic(Species):
                 f'Polarisability: {self.polarisability} m^3\n'
                 f'Multiplicity: {self.multiplicity}\n'
                 f'Effective valence electrons: {self.effectiveelectrons}\n'
-                f'Electron cross section: {self.electroncrosssection} m^2\n'
+                f'Electron cross section data: {self.electroncrosssection}\n'
                 f'Emission lines: {len(self.emissionlines)}')
 
     def partitionfunction_internal(self, T, dE):
