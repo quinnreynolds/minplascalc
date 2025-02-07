27da21cfef17a8dbacad542aa3219a503855c2f5
pag1pag <66677348+pag1pag@users.noreply.github.com>
2025-02-07 16:44:11 +0100

    Update functions_radiation.py
    - run make qa
    - update documentation

--- minplascalc/functions_radiation.py
+++ minplascalc/functions_radiation.py
@@ -1,18 +1,26 @@
+from typing import TYPE_CHECKING
+
 import numpy
-from scipy import constants
 
-kb = constants.Boltzmann
-line_preconst = constants.Planck * constants.c / (4*constants.pi)
+from minplascalc.units import Units
 
-def total_emission_coefficient(mix):
-    """Calculate the LTE total radiation emission coefficient of the plasma 
-    in W/m3.sr.
-    """
+if TYPE_CHECKING:
+    from minplascalc.mixture import LTE
+
+
+u = Units()
+
+
+line_preconst = u.h * u.c / (4 * u.pi)
+
+
+def total_emission_coefficient(mix: "LTE") -> float:
+    """Compute the LTE total radiation emission coefficient of the plasma in W/m3.sr."""
     nd = mix.calculate_composition()
     rt = 0
     for nv, sp in zip(nd[:-1], mix.species[:-1]):
         Qi = sp.partitionfunction_internal(mix.T, 0)
         for eml in sp.emissionlines:
             wl, gA, Ek = eml
-            rt += line_preconst * nv * gA * numpy.exp(-Ek/(kb*mix.T)) / (Qi*wl)
+            rt += line_preconst * nv * gA * numpy.exp(-Ek / (u.k_b * mix.T)) / (Qi * wl)
     return rt
