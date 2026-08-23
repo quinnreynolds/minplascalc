2b7cd6e8297f7db3122c5a2d00c7d2ca01c25d2b
Carl Sandrock <carl.sandrock@brandtraction.co.uk>
2026-08-23 17:01:10 +0200

    Add analytical partition temperature derivatives

--- tests/test_species.py
+++ tests/test_species.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 import minplascalc as mpc
@@ -49,3 +50,41 @@ def test_monatomic_internal_energy(monatomic_sample_species, T, energy, tol):
 def test_diatomic_internal_energy(diatomic_sample_species, T, energy, tol):
     internal_energy = diatomic_sample_species.internal_energy(T, 0)
     assert internal_energy == pytest.approx(energy, abs=tol)
+
+
+@pytest.mark.parametrize("species_name", ["O2", "O+"])
+@pytest.mark.parametrize("T", [2000.0, 12000.0, 25000.0])
+def test_partition_log_temperature_derivative(species_name, T):
+    species = mpc.species.from_name(species_name)
+    volume = 0.37
+    lowering = 0.0
+    delta_T = T * 1e-5
+
+    log_high = np.log(
+        species.total_partition_function(volume, T + delta_T, lowering)
+    )
+    log_low = np.log(
+        species.total_partition_function(volume, T - delta_T, lowering)
+    )
+    finite_difference = (log_high - log_low) / (2 * delta_T)
+
+    assert species.dlog_total_partition_dT(T, lowering) == pytest.approx(
+        finite_difference, rel=2e-8
+    )
+
+
+def test_electron_partition_log_temperature_derivative():
+    electron = mpc.species.Electron()
+    T = 12000.0
+    delta_T = T * 1e-5
+    volume = 0.37
+    log_high = np.log(
+        electron.total_partition_function(volume, T + delta_T, 0.0)
+    )
+    log_low = np.log(
+        electron.total_partition_function(volume, T - delta_T, 0.0)
+    )
+
+    assert electron.dlog_total_partition_dT(T, 0.0) == pytest.approx(
+        (log_high - log_low) / (2 * delta_T), rel=2e-8
+    )
